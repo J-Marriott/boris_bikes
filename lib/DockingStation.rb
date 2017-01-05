c22fc0b8e9946164c5956d256f958490a9ccfd7e
Gurminder Bhachu <gurminderbhachu@hotmail.com>
2017-01-05 17:38:55 +0000

    releases same bike

--- lib/DockingStation.rb
+++ lib/DockingStation.rb
@@ -1,11 +1,13 @@
 require_relative 'bike'
 
 class DockingStation
-    
+
     attr_reader :bike
-    
+
     def release_bike
-        Bike.new
+      fail "No bikes" unless @bike
+
+        @bike
     end
 
     def dock_bike(bike)
--- spec/DockingStation_spec.rb
+++ spec/DockingStation_spec.rb
@@ -6,30 +6,36 @@ describe DockingStation do
 
 end
 
-
-  it "Releases a bike" do
-    bike = subject.release_bike
-    expect(bike).to be_working
-  end
-  
-  it "Responds to dock" do
+it "Responds to dock" do
     expect(subject).to respond_to :dock_bike
   end
-  
+
   it { is_expected.to respond_to(:dock_bike).with(1).argument }
-  
+
   it 'docks a bike' do
     bike = Bike.new
     expect(subject.dock_bike(bike)).to eq bike
   end
-  
+
   it 'responds to bike' do
     expect(subject).to respond_to :bike
   end
-  
+
   it 'returns a docked bike' do
     bike = Bike.new
     subject.dock_bike(bike)
     expect(subject.bike).to eq bike
+  end
+
+describe "#Releases a bike" do
+  it 'releases the same bike' do
+    bike = Bike.new
+    subject.dock_bike(bike)
+    expect(subject.release_bike).to eq bike
+  end
+
+  it "Releases a bike" do
+expect {subject.release_bike}.to raise_error 'No bikes'
+  end
   end
 end
